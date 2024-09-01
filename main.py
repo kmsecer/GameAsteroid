b7b558136d288d84cde154a07d1f7d87218479e8
kmsecer <kmsecer@gmail.com>
2024-09-01 13:45:07 -0700

    all

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
+    clock=pygame.time.Clock()
+    dt=0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+        # limit the framerate to 60 FPS
+        dt=clock.tick(60)/1000
+
+
+if __name__ == "__main__":
+    main()
